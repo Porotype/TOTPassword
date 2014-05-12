7d81c5d134b961ec54d29d08a770f5c330aead39
Matti Tahvonen <matti@vaadin.com>
2014-05-12 18:02:53 +0300

    mavenized and "lightened" the project

--- src/main/java/com/porotype/totpassword/TOTPasswordUI.java
+++ src/main/java/com/porotype/totpassword/TOTPasswordUI.java
@@ -1,11 +1,5 @@
 package com.porotype.totpassword;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Date;
-
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -14,24 +8,29 @@ import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
-
 import fi.jasoft.qrcode.QRCode;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
+import javax.servlet.annotation.WebServlet;
+import org.vaadin.maddon.button.PrimaryButton;
+import org.vaadin.maddon.label.RichText;
+import org.vaadin.maddon.layouts.MHorizontalLayout;
+import org.vaadin.maddon.layouts.MVerticalLayout;
 
 /**
  * @author Marc Englund
- * 
+ *
  */
 @SuppressWarnings("serial")
-@Theme("totpassword")
+@Theme("dawn")
 public class TOTPasswordUI extends UI {
 
     @WebServlet(value = "/*", asyncSupported = true)
@@ -43,20 +42,10 @@ public class TOTPasswordUI extends UI {
 
     @Override
     protected void init(final VaadinRequest request) {
-        final VerticalLayout mainLayout = new VerticalLayout();
-        mainLayout.setSizeUndefined();
-        mainLayout.setMargin(true);
-        mainLayout.setSpacing(true);
-        setContent(mainLayout);
 
         /*
          * 1. Setup
          */
-
-        final HorizontalLayout setupLayout = new HorizontalLayout();
-        setupLayout.setSpacing(true);
-        mainLayout.addComponent(setupLayout);
-
         // Generate secret key, display as QR code and as a plaintext link
         final GoogleAuthenticatorKey key = gAuth.createCredentials();
 
@@ -70,41 +59,29 @@ public class TOTPasswordUI extends UI {
         }
 
         // generate QR code
-        final QRCode qr = new QRCode();
-        qr.setValue(keyUri);
-        qr.setWidth("140px");
-        qr.setHeight("140px");
-        setupLayout.addComponent(qr);
+        final QRCode sharedKeyQrCode = new QRCode();
+        sharedKeyQrCode.setValue(keyUri);
+        sharedKeyQrCode.setWidth("140px");
+        sharedKeyQrCode.setHeight("140px");
 
         // add a key in plain text, as clickable link
-        final Label text = new Label(
-                "Scan the QR code or enter shared key manually into your app. "
-                        + "<h1><a href=\"" + keyUri + "\">" + key.getKey()
-                        + "</a></h1>"
-                        + "Then try the generated password below:",
-                ContentMode.HTML);
-        setupLayout.addComponent(text);
-        setupLayout.setComponentAlignment(text, Alignment.MIDDLE_CENTER);
+        final Label sharedKeyText = new Label(
+                "<h2><a href=\"" + keyUri + "\">" + key.getKey()
+                + "</a></h2>", ContentMode.HTML);
 
         /*
          * 2. Verification
          */
-
-        // Verify password
-        final HorizontalLayout checkLayout = new HorizontalLayout();
-        checkLayout.setSpacing(true);
-        mainLayout.addComponent(checkLayout);
-
         final TextField password = new TextField("Password");
         password.setInputPrompt("123456");
         password.setColumns(6);
-        checkLayout.addComponent(password);
 
-        Button check = new Button("Check", new Button.ClickListener() {
+        Button check = new PrimaryButton("Check", new Button.ClickListener() {
             @Override
             public void buttonClick(ClickEvent event) {
                 String value = password.getValue();
-                Integer totp = Integer.valueOf((value.equals("") ? "-1" : value));
+                Integer totp = Integer.
+                        valueOf((value.equals("") ? "-1" : value));
 
                 // it's a good idea to allow each password only once
                 boolean unused = isUnusedPassword(totp, gAuth.getWindowSize());
@@ -122,9 +99,15 @@ public class TOTPasswordUI extends UI {
 
             }
         });
-        checkLayout.addComponent(check);
-        checkLayout.setComponentAlignment(check, Alignment.BOTTOM_LEFT);
-        mainLayout.setComponentAlignment(checkLayout, Alignment.MIDDLE_CENTER);
+
+        setContent(
+                new MVerticalLayout(
+                        new RichText().withMarkDownResource("/hello.md"),
+                        new MHorizontalLayout(sharedKeyQrCode, sharedKeyText),
+                        new MHorizontalLayout(password, check)
+                            .withHeight("100px").alignAll(Alignment.BOTTOM_CENTER)
+                )
+        );
 
     }
 
@@ -135,15 +118,12 @@ public class TOTPasswordUI extends UI {
      * The format is:
      * otpauth://totp/[ISSUER]:[ACCOUNT]?secret=[SECRET]&issuer=[ISSUER]
      * </p>
-     * 
+     *
      * @see https://code.google.com/p/google-authenticator/wiki/KeyUriFormat
-     * 
-     * @param account
-     *            account/username
-     * @param issuer
-     *            the provider or service this account is associated with
-     * @param secret
-     *            shared secret key
+     *
+     * @param account account/username
+     * @param issuer the provider or service this account is associated with
+     * @param secret shared secret key
      * @return URI string
      * @throws URISyntaxException
      */
@@ -169,11 +149,9 @@ public class TOTPasswordUI extends UI {
      * the allowed timeframe. Note that the same password can certainly be valid
      * in the future, we just want to prevent using it again right away. <br/>
      * <b>This implementation is not perfect.</b>
-     * 
-     * @param password
-     *            the password being verified
-     * @param windowSize
-     *            number of intervals being checked
+     *
+     * @param password the password being verified
+     * @param windowSize number of intervals being checked
      * @return
      */
     private boolean isUnusedPassword(int password, int windowSize) {
